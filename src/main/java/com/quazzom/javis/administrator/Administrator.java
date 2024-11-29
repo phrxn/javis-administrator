c2ef80c44cf611eaac47ddb48c0e1fa14f167970
phrxn <david@quazzom.com>
2024-11-29 15:34:19 -0300

    JAVIS-ADMINISTRATOR 0.1.0-beta

--- src/main/java/com/quazzom/javis/administrator/Administrator.java
+++ src/main/java/com/quazzom/javis/administrator/Administrator.java
@@ -3,7 +3,7 @@ package com.quazzom.javis.administrator;
 public class Administrator {
 
   private static final String PROGRAM_NAME = "Javis administrator";
-  private static final String PROGRAM_VERSION = "0.0.2-SNAPSHOT";
+  private static final String PROGRAM_VERSION = "0.1.0-beta";
 
   private String strProgramName;
   private String strProgramVersion;
